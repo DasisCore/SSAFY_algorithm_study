70c528aa427b7cd5c91400f89b7ceaf08c0af19d
cih831 <choi83195@gmail.com>
2022-03-10 23:10:56 +0900

    0310 Inho

--- Choi_Inho/z.CT/PROBLEM3.py
+++ Choi_Inho/z.CT/PROBLEM3.py
@@ -1,31 +1,3 @@
-# import sys
-# sys.stdin = open('PROBLEM3.txt')
-#
-# N = int(input())
-# D = [input() for _ in range(4)]
-# L = [input() for _ in range(N)]
-#
-# for i in range(len(L)): # 어떤 단어인지?
-#     visited = [0] * 4
-#     cnt = 0
-#     for j in range(len(L[i])): # 그 단어에서 글자를 순회
-#         for k in range(len(D)): # 주사위를 순회
-#             for l in range(len(D[k])): # 주사위 내부의 글자를 순회
-#                 if L[i][j] == D[k][l] and not visited[k]: # 주사위 내부의 글자와 단어의 글자가 같고, 방문이 안찍혀있으면
-#                     visited[k] = 1                        # 방문찍고 cnt 올리고 break
-#                     cnt += 1
-#                     break
-#                 elif visited[k]: # 주사위 방문이 이미 찍혀있으면 그냥 break로 다음 주사위 봄
-#                     break
-#             if cnt == len(L[i]): # 주사위 방
-#                 break
-#         if cnt == len(L[i]):
-#             break
-#     if cnt == len(L[i]):
-#         print('YES')
-#     else:
-#         print('NO')
-##################################
 import sys
 sys.stdin = open('PROBLEM3.txt')
 
@@ -53,6 +25,34 @@ for i in range(len(L)): # 어떤 단어인지?
         print('YES')
     else:
         print('NO')
+##################################
+# import sys
+# sys.stdin = open('PROBLEM3.txt')
+#
+# N = int(input())
+# D = [input() for _ in range(4)]
+# L = [input() for _ in range(N)]
+#
+# for i in range(len(L)): # 어떤 단어인지?
+#     visited = [0] * 4
+#     cnt = 0
+#     for j in range(len(L[i])): # 그 단어에서 글자를 순회
+#         for k in range(len(D)): # 주사위를 순회
+#             for l in range(len(D[k])): # 주사위 내부의 글자를 순회
+#                 if L[i][j] == D[k][l] and not visited[k]: # 주사위 내부의 글자와 단어의 글자가 같고, 방문이 안찍혀있으면
+#                     visited[k] = 1                        # 방문찍고 cnt 올리고 break
+#                     cnt += 1
+#                     break
+#                 elif visited[k]: # 주사위 방문이 이미 찍혀있으면 그냥 break로 다음 주사위 봄
+#                     break
+#             if cnt == len(L[i]): # 주사위 방
+#                 break
+#         if cnt == len(L[i]):
+#             break
+#     if cnt == len(L[i]):
+#         print('YES')
+#     else:
+#         print('NO')
 
 
 ###################################
