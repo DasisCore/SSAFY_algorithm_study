e8ff006995967b748e454c00b27233c3b77343db
cih831 <choi83195@gmail.com>
2022-03-13 23:06:32 +0900

    0313 Inho

---
+++ Choi_Inho/silver/1012.py
@@ -0,0 +1,11 @@
+import sys
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    arr = [[0]*M for _ in range(N)]
+    for i in range(K):
+        x, y = map(int, sys.stdin.readline().split())
+        arr[y][x] = 1
+
+    for
