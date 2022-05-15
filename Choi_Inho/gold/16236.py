b2dc3c5ce45518bb69bae74d49d0cbd091510bd0
cih831 <choi83195@gmail.com>
2022-05-15 23:52:34 +0900

    0515 Inho

--- Choi_Inho/gold/16236.py
+++ Choi_Inho/gold/16236.py
@@ -1,14 +1,4 @@
-# N = int(input())
-# arr = [list(map(int, input().split())) for _ in range(N)]
-# for i in range(N):
-#
-#
-
-
-X, Y = map(int, input().split())
-my_max = 0
-
-for h in range(min(X, Y)):
-    my_max = max(my_max, (X - 2 * h) * (Y - 2 * h) * h)
-
-print(my_max)
+N = int(input())
+arr = [list(map(int, input().split())) for _ in range(N)]
+for i in range(N):
+    pass
