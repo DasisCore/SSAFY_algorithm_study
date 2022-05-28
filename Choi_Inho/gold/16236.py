15504f3332fbc8bce2de8471652d909ae087031d
cih831 <choi83195@gmail.com>
2022-05-28 21:32:36 +0900

    0528 Inho

--- Choi_Inho/gold/16236.py
+++ Choi_Inho/gold/16236.py
@@ -1,3 +1,8 @@
-N = int(input())
-arr = [list(map(int, input().split())) for _ in range(N)]
-for i in range(N):
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+# for i in range(N):
+
+
+
+a = []
+print(dir(a))
